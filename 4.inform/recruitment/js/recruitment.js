78233a8ab2bb1b13a1ef0b20846baeaa45dd2240
jeonghae98 <answjdgo8@gmail.com>
2024-07-01 15:56:24 +0900

    Add files via upload

---
+++ 4.inform/recruitment/js/recruitment.js
@@ -0,0 +1,10 @@
+$(function() {
+    //===================== main =====================
+     // <공통 애니메이션>
+     $('.inform-title').css('opacity', '1');
+     $('.menu-list').css('opacity', '1');
+     $('.content-box').css({
+        'opacity': 1,
+        'transform': 'translateY(0)'
+     });
+});
